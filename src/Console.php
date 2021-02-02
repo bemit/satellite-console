f1679edca997fe4fbe3b8250072b274bd29a2574
Michael Becker <michael@bemit.codes>
2021-02-02 10:14:46 +0100

    ooptimize getop pass down

--- src/Console.php
+++ src/Console.php
@@ -10,35 +10,37 @@ class Console {
      * @var array $commands
      */
     protected static $commands = [];
+    protected GetOpt $get_opt;
+
+    public function __construct(GetOpt $get_opt) {
+        $this->get_opt = $get_opt;
+    }
 
     public static function addCommand($name, \GetOpt\Command $command) {
         static::$commands[$name] = $command;
     }
 
     /**
-     * @param GetOpt $get_opt
-     *
      * @return \GetOpt\Command
      * @throws \GetOpt\ArgumentException
-     *
      */
-    public function process(GetOpt $get_opt): \GetOpt\Command {
-        $get_opt->addOption((new Option('h', 'help', Getopt::NO_ARGUMENT))->setDescription('Displays help with all commands.'));
+    public function process(): \GetOpt\Command {
+        $this->get_opt->addOption((new Option('h', 'help', Getopt::NO_ARGUMENT))->setDescription('Displays help with all commands.'));
 
         foreach(static::$commands as $cmd) {
-            $get_opt->addCommand($cmd);
+            $this->get_opt->addCommand($cmd);
         }
 
-        $get_opt->process();// throws: \GetOpt\ArgumentException
+        $this->get_opt->process();// throws: \GetOpt\ArgumentException
 
-        if($get_opt->getOption('h')) {
-            echo $get_opt->getHelpText();
+        if($this->get_opt->getOption('h')) {
+            echo $this->get_opt->getHelpText();
             echo 'Show options, arguments and info about one command: ' . PHP_EOL;
             echo '  php cli <command-name> -h ' . PHP_EOL . PHP_EOL;
             exit;
         }
 
-        $command = $get_opt->getCommand();
+        $command = $this->get_opt->getCommand();
         if(!$command) {
             // no command
             error_log('Console: no command');
